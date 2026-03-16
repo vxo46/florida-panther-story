f32ae8c980f1d197f2923350bcb9757bb6ff89b9
vxo46 <vxo46@miami.edu>
2026-03-16 13:19:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,69 @@
-const width = 700;
-const height = 500;
+// MAP (simple animated fade layers)
 
-const svg = d3.select("#heatmap")
-  .append("svg")
-  .attr("viewBox", `0 0 ${width} ${height}`);
+const map = d3.select("#map")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", 500);
 
-const gridSize = 20;
+const layers = [
+    {color:"#ffcc80", opacity:0.9},
+    {color:"#ff8a65", opacity:0.6},
+    {color:"#e65100", opacity:0.4},
+    {color:"#bf360c", opacity:0.2}
+];
 
-const years = {
-  1975: generateData(10),
-  1995: generateData(25),
-  2010: generateData(50),
-  2020: generateData(80)
-};
+layers.forEach((layer, i) => {
+    map.append("rect")
+        .attr("x", 50 + i*40)
+        .attr("y", 50 + i*40)
+        .attr("width", 400 - i*80)
+        .attr("height", 300 - i*80)
+        .attr("fill", layer.color)
+        .attr("opacity", layer.opacity);
+});
 
-const color = d3.scaleSequential()
-  .interpolator(d3.interpolateReds)
-  .domain([0, 100]);
+// POPULATION CHART
 
-function generateData(intensity) {
-  return d3.range(400).map(() => Math.random() * intensity);
-}
+const data = [
+    {year:1970, pop:30},
+    {year:1980, pop:30},
+    {year:1990, pop:40},
+    {year:2000, pop:80},
+    {year:2010, pop:120},
+    {year:2020, pop:200}
+];
 
-function draw(year) {
-  const data = years[year];
+const width = 600;
+const height = 400;
 
-  svg.selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("x", (d, i) => (i % 20) * gridSize)
-    .attr("y", (d, i) => Math.floor(i / 20) * gridSize)
-    .attr("width", gridSize)
-    .attr("height", gridSize)
-    .transition()
-    .duration(800)
-    .attr("fill", d => color(d));
-}
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-draw(1975);
+const x = d3.scaleLinear()
+    .domain([1970, 2020])
+    .range([50, width - 50]);
 
-// Scrollama
-const scroller = scrollama();
+const y = d3.scaleLinear()
+    .domain([0, 220])
+    .range([height - 50, 50]);
 
-scroller
-  .setup({
-    step: ".step",
-    offset: 0.6
-  })
-  .onStepEnter(response => {
-    const year = response.element.dataset.year;
-    draw(year);
-  });
+const line = d3.line()
+    .x(d => x(d.year))
+    .y(d => y(d.pop));
+
+svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "#ff8a65")
+    .attr("stroke-width", 3)
+    .attr("d", line);
+
+svg.append("g")
+    .attr("transform", `translate(0,${height-50})`)
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+svg.append("g")
+    .attr("transform", `translate(50,0)`)
+    .call(d3.axisLeft(y));
