98e386abd8a736b09d8a96868f0c4c24bbdef465
vxo46 <vxo46@miami.edu>
2026-03-16 17:35:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,24 @@
 const mapWidth = document.querySelector(".sticky").clientWidth;
 const mapHeight = window.innerHeight;
 
-const svg = d3.select("#map")
+const mapSvg = d3.select("#map")
   .attr("width", mapWidth)
   .attr("height", mapHeight);
 
 const projection = d3.geoMercator();
 const path = d3.geoPath().projection(projection);
 
-d3.json("florida.geo.json").then(function(data) {
+d3.json("florida.geo.json").then(data => {
 
   projection.fitSize([mapWidth, mapHeight], data);
 
-  const base = svg.append("path")
+  const base = mapSvg.append("path")
     .datum(data)
     .attr("d", path)
     .attr("fill", "#ddd")
     .attr("stroke", "#333");
 
-  const overlay = svg.append("path")
+  const overlay = mapSvg.append("path")
     .datum(data)
     .attr("d", path)
     .attr("fill", "#ffb74d")
@@ -49,45 +49,71 @@ d3.json("florida.geo.json").then(function(data) {
    POPULATION CHART
 ====================== */
 
-const chartWidth = 800;
-const chartHeight = 400;
+d3.csv("population.csv").then(data => {
 
-const chartSvg = d3.select("#chart")
-  .attr("width", chartWidth)
-  .attr("height", chartHeight);
+  data.forEach(d => {
+    d.year = +d.year;
+    d.population = +d.population;
+  });
 
-const population = [
-  {year:1970, pop:30},
-  {year:1980, pop:30},
-  {year:1990, pop:40},
-  {year:2000, pop:80},
-  {year:2010, pop:120},
-  {year:2020, pop:200}
-];
+  const width = 900;
+  const height = 500;
 
-const x = d3.scaleLinear()
-  .domain([1970, 2020])
-  .range([60, chartWidth-40]);
+  const svg = d3.select("#chart")
+    .attr("width", width)
+    .attr("height", height);
 
-const y = d3.scaleLinear()
-  .domain([0, 220])
-  .range([chartHeight-60, 40]);
+  const x = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.year))
+      .range([80, width - 50]);
 
-const line = d3.line()
-  .x(d => x(d.year))
-  .y(d => y(d.pop));
+  const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.population) + 20])
+      .range([height - 80, 40]);
 
-chartSvg.append("path")
-  .datum(population)
-  .attr("fill","none")
-  .attr("stroke","#ef6c00")
-  .attr("stroke-width",3)
-  .attr("d", line);
+  svg.append("g")
+    .attr("transform", `translate(0, ${height - 80})`)
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
-chartSvg.append("g")
-  .attr("transform","translate(0,"+(chartHeight-60)+")")
-  .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+  svg.append("g")
+    .attr("transform", "translate(80,0)")
+    .call(d3.axisLeft(y));
 
-chartSvg.append("g")
-  .attr("transform","translate(60,0)")
-  .call(d3.axisLeft(y));
+  const line = d3.line()
+    .x(d => x(d.year))
+    .y(d => y(d.population));
+
+  const path = svg.append("path")
+    .datum([])
+    .attr("fill", "none")
+    .attr("stroke", "#ef6c00")
+    .attr("stroke-width", 4);
+
+  const chartScroller = scrollama();
+
+  chartScroller
+    .setup({
+      step: ".chart-step",
+      offset: 0.6
+    })
+    .onStepEnter(response => {
+
+      let visibleData;
+
+      if (response.index === 0) {
+        visibleData = data.filter(d => d.year <= 1973);
+      } else if (response.index === 1) {
+        visibleData = data.filter(d => d.year <= 1995);
+      } else if (response.index === 2) {
+        visibleData = data.filter(d => d.year <= 2010);
+      } else {
+        visibleData = data;
+      }
+
+      path
+        .datum(visibleData)
+        .transition()
+        .duration(800)
+        .attr("d", line);
+    });
+});
