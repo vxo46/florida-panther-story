f566d04f68992b0180ed40cc9a5e6cb852fbea83
vxo46 <vxo46@miami.edu>
2026-03-16 13:40:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,98 @@
-// MAP (simple animated fade layers)
+/* ======================
+   MAP SETUP
+====================== */
 
-const map = d3.select("#map")
-    .append("svg")
-    .attr("width", "100%")
-    .attr("height", 500);
+const mapWidth = document.querySelector(".sticky").clientWidth;
+const mapHeight = window.innerHeight;
 
-const layers = [
-    {color:"#ffcc80", opacity:0.9},
-    {color:"#ff8a65", opacity:0.6},
-    {color:"#e65100", opacity:0.4},
-    {color:"#bf360c", opacity:0.2}
-];
+const svg = d3.select("#map")
+  .attr("width", mapWidth)
+  .attr("height", mapHeight);
 
-layers.forEach((layer, i) => {
-    map.append("rect")
-        .attr("x", 50 + i*40)
-        .attr("y", 50 + i*40)
-        .attr("width", 400 - i*80)
-        .attr("height", 300 - i*80)
-        .attr("fill", layer.color)
-        .attr("opacity", layer.opacity);
+const projection = d3.geoMercator();
+const path = d3.geoPath().projection(projection);
+
+/* ======================
+   LOAD FLORIDA
+====================== */
+
+d3.json("florida.geo.json").then(function(data) {
+
+  projection.fitSize([mapWidth, mapHeight], data);
+
+  const base = svg.append("path")
+    .datum(data)
+    .attr("d", path)
+    .attr("fill", "#333")
+    .attr("stroke", "#aaa");
+
+  const overlay = svg.append("path")
+    .datum(data)
+    .attr("d", path)
+    .attr("fill", "#ffcc80")
+    .attr("opacity", 0.8);
+
+  const colors = ["#ffcc80","#ff8a65","#e65100","#bf360c"];
+
+  const scroller = scrollama();
+
+  scroller
+    .setup({
+      step: ".step",
+      offset: 0.5
+    })
+    .onStepEnter(response => {
+      overlay
+        .transition()
+        .duration(600)
+        .attr("fill", colors[response.index]);
+    });
+
 });
 
-// POPULATION CHART
+/* ======================
+   POPULATION CHART
+====================== */
 
-const data = [
-    {year:1970, pop:30},
-    {year:1980, pop:30},
-    {year:1990, pop:40},
-    {year:2000, pop:80},
-    {year:2010, pop:120},
-    {year:2020, pop:200}
+const chartWidth = 800;
+const chartHeight = 400;
+
+const chartSvg = d3.select("#chart")
+  .attr("width", chartWidth)
+  .attr("height", chartHeight);
+
+const population = [
+  {year:1970, pop:30},
+  {year:1980, pop:30},
+  {year:1990, pop:40},
+  {year:2000, pop:80},
+  {year:2010, pop:120},
+  {year:2020, pop:200}
 ];
 
-const width = 600;
-const height = 400;
-
-const svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
 const x = d3.scaleLinear()
-    .domain([1970, 2020])
-    .range([50, width - 50]);
+  .domain([1970, 2020])
+  .range([60, chartWidth-40]);
 
 const y = d3.scaleLinear()
-    .domain([0, 220])
-    .range([height - 50, 50]);
+  .domain([0, 220])
+  .range([chartHeight-60, 40]);
 
 const line = d3.line()
-    .x(d => x(d.year))
-    .y(d => y(d.pop));
+  .x(d => x(d.year))
+  .y(d => y(d.pop));
 
-svg.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "#ff8a65")
-    .attr("stroke-width", 3)
-    .attr("d", line);
+chartSvg.append("path")
+  .datum(population)
+  .attr("fill","none")
+  .attr("stroke","#ff8a65")
+  .attr("stroke-width",3)
+  .attr("d", line);
 
-svg.append("g")
-    .attr("transform", `translate(0,${height-50})`)
-    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+chartSvg.append("g")
+  .attr("transform","translate(0,"+(chartHeight-60)+")")
+  .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
-svg.append("g")
-    .attr("transform", `translate(50,0)`)
-    .call(d3.axisLeft(y));
+chartSvg.append("g")
+  .attr("transform","translate(60,0)")
+  .call(d3.axisLeft(y));
